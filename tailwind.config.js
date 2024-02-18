dc8460375bd9f94d2e8f38ba3244dbb7bcc6f415
TK4464 <4464tk64@gmail.com>
2024-02-19 00:32:37 +0900

    最新版

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,7 @@ export default {
         },
     },
     plugins: [
-        require("@tailwindcss/line-clamp"),
+        
         //...
         function ({ addUtilities }) {
             const newUtilities = {
